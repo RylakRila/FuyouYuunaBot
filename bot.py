f8a881852ec0d6a99d1ccc8ea877bc7198df175a
RylakRila <kylvark@gmail.com>
2022-04-24 10:22:39 +0800

    update prefix data with json

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
+import json
 import discord
 from discord.ext import commands
 
-yuyuyu_bot = commands.Bot(command_prefix='/')
-token = open("./token.txt", "r").readline()
+with open("data.json", "r") as jsonTemp:
+    jsonData = json.load(jsonTemp)
+
+print(jsonData['prefix'])
+
+yuyuyu_bot = commands.Bot(command_prefix=jsonData['prefix'])
 
 @yuyuyu_bot.event
 async def on_ready():
