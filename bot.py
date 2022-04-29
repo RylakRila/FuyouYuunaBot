3e8b6a63891011a973efe1c717009d84ffba9319
RylakRila <kylvark@gmail.com>
2022-04-29 21:43:49 +0800

    bot debugging

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from dis import disco
 import json
 import os
 import discord
@@ -6,7 +7,7 @@ from discord.ext import commands
 with open("data.json", "r") as jsonTemp:
     jsonData = json.load(jsonTemp)
 
-yuyuyu = commands.Bot(command_prefix=jsonData['prefix'])
+yuyuyu = commands.Bot(command_prefix=jsonData['prefix'], intents=discord.Intents.all())
 
 @yuyuyu.event
 async def on_ready():
@@ -16,5 +17,6 @@ async def on_ready():
 async def on_member_join(member):
     channel = yuyuyu.get_channel(jsonData["welcome-channel"])
     await channel.send(f"{member}加入了频道，我们鼓掌。")
+    print(f"{member} joined server!")
 
 yuyuyu.run(os.environ['DPY_TOKEN'])
