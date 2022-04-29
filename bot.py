e4f4ca157ac3c2e260f22522f696f3ee13c3105f
RylakRila <kylvark@gmail.com>
2022-04-29 22:26:21 +0800

    Add function of welcome new member

--- bot.py
+++ bot.py
@@ -19,8 +19,4 @@ async def on_member_join(member):
     await channel.send(f"{member}加入了频道，我们鼓掌。")
     print(f"{member} joined server!")
 
-@yuyuyu.event
-async def on_member_remove(member):
-    pass
-
 yuyuyu.run(os.environ['DPY_TOKEN'])
