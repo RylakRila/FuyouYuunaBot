da18cc0d108a700a2b9c268cc11fe74dcc28b2b6
RylakRila <kylvark@gmail.com>
2022-05-03 22:30:25 +0800

    test deleting multiple messages feature

--- bot.py
+++ bot.py
@@ -18,13 +18,19 @@ async def on_member_join(member):
     channel = yuyuyu.get_channel(jsonData["welcome_channel"])
     await channel.send(f"{member} 加入了频道，我们鼓掌。")
 
-@yuyuyu.command()
+@yuyuyu.command(name="ping", brief="Retrieve the latency of bot in millisecond")
 async def ping(ctx):
     await ctx.send(f"{round(yuyuyu.latency*1000)}(ms)")
 
-@yuyuyu.command()
+@yuyuyu.command(name="YuYuYu meme", brief="Randomly post a meme of YuYuYu anime in channel")
 async def yuyuyumeme(ctx):
     random_img = random.choice(jsonData["meme.yuyuyu"])
     await ctx.send(random_img)
+
+@yuyuyu.command(name="Delete Multiple Message", brief="Delete multiple messages of a give number")
+async def clear(ctx, amount=1):
+    if ctx.message.author.id == ctx.guild.owner_id:
+        amount += 1
+        await ctx.channel.purge(limit=amount)
 
 yuyuyu.run(os.environ['DPY_TOKEN'])
