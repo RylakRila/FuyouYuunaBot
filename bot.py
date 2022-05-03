cc17b9a751821969e810d8fde61a6782ec980b6f
RylakRila <kylvark@gmail.com>
2022-05-03 20:18:58 +0800

    Add sending image feature

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import json
+from msilib.schema import File
 import os
 import discord
+import random
 from discord.ext import commands
 
-with open("data.json", "r") as jsonTemp:
+with open("data.json", "r", encoding="utf-8") as jsonTemp:
     jsonData = json.load(jsonTemp)
 
 yuyuyu = commands.Bot(command_prefix=jsonData['prefix'], intents=discord.Intents.all())
@@ -20,5 +22,11 @@ async def on_member_join(member):
 @yuyuyu.command()
 async def ping(ctx):
     await ctx.send(f"{round(yuyuyu.latency*1000)}(ms)")
+
+@yuyuyu.command()
+async def yuyuyumeme(ctx):
+    img_number = random.randint(0, 11)
+    img = discord.File(jsonData["meme.yuyuyu"][img_number])
+    await ctx.send(File=img)
 
 yuyuyu.run(os.environ['DPY_TOKEN'])
