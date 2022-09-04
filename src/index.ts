c18bef34b1e998c8b0c13f2324559ece9b00153f
RylakRila <kylvark@gmail.com>
2022-09-04 15:16:41 -0400

    added welcome function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, TextChannel } from "discord.js";
 import * as dotenv from 'dotenv';
+import config from './config.json';
 
 const yuyuyuBot = new Client({intents: [
     GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages
 ]});
 
 yuyuyuBot.on('ready', () => {
     console.log('Ready!');
+});
+
+yuyuyuBot.on("guildMemberAdd", async member => {
+    let welcomeChannel = yuyuyuBot.channels.cache.get(config.welcome_channel) as TextChannel;
+    await welcomeChannel.send(`${member}加入了频道，我们鼓掌。`);
 });
 
 dotenv.config();
